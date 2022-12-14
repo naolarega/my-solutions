1431a92604ebd5d92de17c6e965832427932c103
Naol Arega <naol.dev@gmail.com>
2022-12-14 21:48:34 +0300

    fix: domino piling

--- src/codeforces/domino_piling.rs
+++ src/codeforces/domino_piling.rs
@@ -4,18 +4,16 @@ pub struct Solution;
 
 impl Solution {
     pub fn domino_piling() {
-        let board_dimension = Self::accept_board_size().unwrap();
+        let board_dimension = Self::accept_board_size();
 
         let mut game_board = GameBoard::new(board_dimension);
 
         let maximum_dominos = game_board.max_dominos();
 
-        println!("Your board can only fit {maximum_dominos} dominos");
+        println!("{maximum_dominos}");
     }
 
-    fn accept_board_size() -> Result<BoardDimension, ()> {
-        use std::cmp::Ordering::*;
-
+    fn accept_board_size() -> BoardDimension {
         let mut board_size_input = String::new();
 
         stdin()
@@ -24,23 +22,23 @@ impl Solution {
 
         let ref mut split_board_size = board_size_input.split(" ");
 
-        if let Equal = split_board_size.count().cmp(&2) {
-            let length_str = split_board_size.next();
-            let width_str = split_board_size.next();
-            
-            let length = Self::parse_dimension(length_str);
-            let width = Self::parse_dimension(width_str);
+        let length_str = split_board_size.next();
+        let width_str = split_board_size.next();
+    
+        let length = Self::parse_dimension(length_str);
+        let width = Self::parse_dimension(width_str);
 
-            Ok(BoardDimension { length, width })
-        } else {
-            Err(())
-        }
+        BoardDimension { length, width }
     }
 
-    fn parse_dimension(dimension: Option<&str>) -> i8 {
+    fn parse_dimension(dimension: Option<&str>) -> i16 {
         if let Some(dimension) = dimension {
-            let dimension: i8 = dimension.parse().unwrap();
-            return  dimension;
+            let dimension: i16 = dimension
+                .trim()
+                .parse()
+                .unwrap();
+
+            return dimension;
         } else {
             return 0;
         }
@@ -48,52 +46,60 @@ impl Solution {
 }
 
 struct BoardDimension {
-    length: i8,
-    width: i8
+    length: i16,
+    width: i16,
 }
 
-struct Domino(i8, i8);
+struct Domino(i16, i16);
 
-impl From<(i8, i8)> for Domino {
-    fn from(coordinate: (i8, i8)) -> Self {
+impl From<(i16, i16)> for Domino {
+    fn from(coordinate: (i16, i16)) -> Self {
         Self(coordinate.0, coordinate.1)
     }
 }
 
 struct GameBoard {
     board_dimension: BoardDimension,
-    dominos: Vec<Domino>
+    dominos: Vec<Domino>,
 }
 
 impl GameBoard {
     fn new(board_dimension: BoardDimension) -> Self {
-        Self{ 
+        Self {
             board_dimension,
-            dominos: Vec::new()
+            dominos: Vec::new(),
         }
     }
 
-    fn max_dominos(&mut self) -> i8 {
+    fn max_dominos(&mut self) -> i16 {
         if !self.board_valid() {
             panic!("board dimensions are not valid!");
         }
-        
-        let ref mut coordinate = (0, 0);
 
+        let ref mut coordinate = (0, 0);
+    
         while self.can_fit_domino(coordinate) {
             self.dominos.push(Domino::from(*coordinate))
         }
 
-        return self.dominos.len() as i8;
+        return self.dominos.len() as i16;
     }
 
-    fn can_fit_domino(&self, coordinate: &mut (i8, i8)) -> bool {
-        false
+    fn can_fit_domino(&self, coordinate: &mut (i16, i16)) -> bool {
+        let left_space = (self.board_dimension.length * self.board_dimension.width) - (self.dominos.len() * 2) as i16;
+
+        if left_space >= 2 {
+            coordinate.0 = 0;
+            coordinate.1 = 0;
+            
+            return true;
+        }
+        return false;
     }
 
     fn board_valid(&self) -> bool {
-        self.board_dimension.length >= 1 &&
-        self.board_dimension.length < self.board_dimension.width &&
-        self.board_dimension.width <= 16
+        self.board_dimension.length >= 1
+            && self.board_dimension.length <= self.board_dimension.width
+            && self.board_dimension.width <= 16
     }
 }
