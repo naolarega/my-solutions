93c66e495749b89f6b1779836beb2524341892e0
naol arega <naol.dev@gmail.com>
2023-10-22 16:00:53 +0300

    feat: roman to integer

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -2,6 +2,7 @@ pub mod fizzbuzz;
 pub mod k_closest;
 pub mod merge_intervals;
 pub mod palindrome_number;
+pub mod roman_to_integer;
 pub mod smaller_numbers;
 pub mod sort_colors;
 pub mod sorting_the_sentence;
---
+++ src/leetcode/roman_to_integer.rs
@@ -0,0 +1,68 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn roman_to_int(s: String) -> i32 {
+        let mut roman_buffer: Option<char> = None;
+        let mut integer_value = 0;
+        let mut temp_intger_value = 0;
+
+        let roman_integer_map = HashMap::from(
+            [
+                ('I',   1),
+                ('V',   5),
+                ('X',   10),
+                ('L',   50),
+                ('C',   100),
+                ('D',   500),
+                ('M',   1000)
+            ]
+        );
+
+        for i in s.chars() {
+            let cur = roman_integer_map.get(&i).unwrap();
+
+            if let Some(roman_buffer) = roman_buffer {
+                let prev = roman_integer_map.get(&roman_buffer).unwrap();
+
+                dbg!(prev, cur);
+                
+                if prev < cur {
+                    temp_intger_value = cur - prev;
+                } else if prev == cur {
+                    temp_intger_value += cur;
+                } else {
+                    integer_value += temp_intger_value;
+                    temp_intger_value = *cur;
+                }
+            } else {
+                temp_intger_value += cur;
+            }
+
+            roman_buffer = Some(i);
+        }
+
+        integer_value + temp_intger_value
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_roman_to_int() {
+        assert_eq!(
+            Solution::roman_to_int(String::from("III")), 3
+        );
+
+        assert_eq!(
+            Solution::roman_to_int(String::from("LVIII")), 58
+        );
+
+        assert_eq!(
+            Solution::roman_to_int(String::from("MCMXCIV")), 1994
+        );
+    }
+}
